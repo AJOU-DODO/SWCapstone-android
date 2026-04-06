db32df874eadf4a1111cee70007cd4ddb0b3181e
apdlwjwjwj <apdlwjwjwj@gmail.com>
2026-04-06 19:10:41 +0900

    style: 불필요한 import 제거

--- app/src/main/java/com/example/swcapstone_android/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/example/swcapstone_android/ui/splash/SplashScreen.kt
@@ -1,6 +1,5 @@
 package com.example.swcapstone_android.ui.splash
 
-import android.window.SplashScreen
 import com.example.swcapstone_android.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -11,12 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.swcapstone_android.ui.Greeting
 import com.example.swcapstone_android.ui.theme.SWCapstoneandroidTheme
 import kotlinx.coroutines.delay
 
@@ -32,14 +29,13 @@ fun SplashScreen(onTimeout: () -> Unit) {
         modifier = Modifier
             .fillMaxSize()
             .background(
-                colorResource(id = R.color.dodo)), // 앱 테마에 맞는 배경색 설정
+                colorResource(id = R.color.dodo)),
         contentAlignment = Alignment.Center
     ) {
-        // 네가 말한 ic_launcher_foreground를 불러와서 배치
         Image(
             painter = painterResource(id = R.mipmap.ic_launcher_foreground),
             contentDescription = "App Logo",
-            modifier = Modifier.size(150.dp) // 로고 크기 조절
+            modifier = Modifier.size(150.dp)
         )
     }
 }
